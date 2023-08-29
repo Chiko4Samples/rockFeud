9c8d31c4cb7cd4d9529412efaf30a12dd7f1f7de
Chiko4Samples <ryshiko_25@yahoo.com>
2023-08-30 03:19:18 +0900

    day2

--- js/main.js
+++ js/main.js
@@ -1,27 +1,64 @@
-var gOvr = false; // finish flg
+var gmStart = false; // games start
+var hasWnr = false; // games stop
+var curPl = 1; // current player
+
+// scores:: maybe not needed
+var pl1Sr = 0;
+var pl2Sr = 0;
+
+// solider cnts
+var maxSl = 13;
+var pl1Sl, pl2Sl = maxSl;
+
+// triggerKeys
+var pl1Ky = 'a';
+var pl2Ky = 'l';
+
+// indicatorBox 
+var ibLoc = 0;
+
+
+// temp
+var triesCnt = 0;
 
 //canvas init
 const dCanvas = document.getElementById('dCanvas');
 const ctx = dCanvas.getContext("2d");
+            
+// key events
+document.addEventListener("keyup", somethingUp, false);
+document.addEventListener("keydown", somethingUp, false);
 
+function somethingUp(e) {
+    // start game
+    if (gmStart == false && e.key == 'y' || e.key == 'Y') {
+        dCanvas.style.background = '#ccc';
+        gmStart = true;
+        setInterval(playOn, 10);
+    }
 
-/*
-setInterval(function() {
-    // prompt start
-    document.onkeydown = function (e) {
-        e = e || window.event;
-        if (e.key == 'y' || e.key == 'Y') {
-            dCanvas.style.background = '#ccc';
+    // attackKeys
+    var triggerKy = (curPl == 1) ? pl1Ky : pl2Ky;
+    if (gmStart == true && hasWnr == false && e.key == triggerKy.toLowerCase()) {
+        console.log([e, e.key]);
+        attack(curPl);
+    }
+}
 
-            // game start
-            init();
-        }
-    };
-}, 10);*/
-init();
+setInterval(playOn, 10);
 
 // init object
-function init() {
+function playOn() {
+    // parapara mangga effect
+    ctx.clearRect(0, 0, dCanvas.width, dCanvas.height);
+
+    // draw current box
+    ctx.beginPath();
+    ibLoc = (curPl == 1) ? 2: 249;
+    ctx.rect(ibLoc, 1, 50, 30);
+    ctx.stroke();
+    ctx.closePath();
+    
     // draw wall
     ctx.beginPath();
     ctx.rect(145, 100, 10, 75);
@@ -58,5 +95,29 @@ function init() {
     ctx.rect(210, 130, 20, 20);
     ctx.fillStyle = '#0000FF';
     ctx.fill();
-    ctx.closePath();
+    ctx.closePath();      
+    
+    // sling and stone over here
+    //drawSling();
+    //drawStone();
+}
+
+
+function attack(pl) {
+    // release rock
+    release(curPl);
+
+    // temp scoring
+    triesCnt++;
+    curPl = (curPl == 1) ? 2: 1;
+    if (triesCnt == 10) {
+        hasWnr = true;
+    }
+}
+
+
+function release(pl) {
+    // get tip of arrow(which arrow was used and which side to attach is based on pl)
+    // calculate trajectory from tip of arrow
+    // calculate number of deminished soldiers from opponent side through rocks coordinates
 }
