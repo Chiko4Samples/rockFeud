899abbe6e18c37f0fcc619470465bc6a84f25300
Chiko4Samples <ryshiko_25@yahoo.com>
2023-09-10 05:31:10 +0900

    castle walls

--- js/main.js
+++ js/main.js
@@ -14,6 +14,16 @@ var pl1Sl, pl2Sl = maxSl;
 var pl1Ky = 'a';
 var pl2Ky = 'l';
 
+// walls
+// wall ctr
+var maxW = 80;
+var w1 = [];
+for (var i = 1; i <= maxW; i++) {
+    w1[i] = {x:0, y:0, c: 0};
+}
+var w2 = w1;
+
+
 // indicatorBox 
 var ibLoc = 0;
 
@@ -60,42 +70,31 @@ function playOn() {
     ctx.closePath();
     
     // draw wall
-    ctx.beginPath();
+    /*ctx.beginPath();
     ctx.rect(145, 100, 10, 75);
     ctx.fillStyle = '#a5700d';
     ctx.fill();
-    ctx.closePath();
+    ctx.closePath();*/
 
     // draw counter left
     // draw counter right
 
-    // draw house left
-    ctx.beginPath();
-    ctx.rect(0, 60, 50, 90);
-    ctx.fillStyle = '#FF0000';
-    ctx.fill();
-    ctx.closePath();
-
-    // draw house right
-    ctx.beginPath();
-    ctx.rect(250, 60, 50, 90);
-    ctx.fillStyle = '#0000FF';
-    ctx.fill();
-    ctx.closePath();
+    // draw house left & right
+    drawHouses(w1, w2);
 
     // draw weapon left
     ctx.beginPath();
-    ctx.rect(70, 130, 20, 20);
+    ctx.rect(65, 128, 10, 20);
     ctx.fillStyle = '#FF0000';
     ctx.fill();
     ctx.closePath();
     
     // draw weapon right
     ctx.beginPath();
-    ctx.rect(210, 130, 20, 20);
+    ctx.rect(225, 128, 10, 20);
     ctx.fillStyle = '#0000FF';
     ctx.fill();
-    ctx.closePath();      
+    ctx.closePath();    
     
     // sling and stone over here
     //drawSling();
@@ -103,7 +102,7 @@ function playOn() {
 }
 
 
-function attack(pl) {
+function attack(curPl) {
     // release rock
     release(curPl);
 
@@ -112,6 +111,50 @@ function attack(pl) {
     curPl = (curPl == 1) ? 2: 1;
     if (triesCnt == 10) {
         hasWnr = true;
+    }
+}
+
+function drawHouses(w1, w2) {
+    // left
+    var lx = 2;
+    var ly = 70;
+    var wp = 8;
+    var wpl = 8;
+    for (var i = 1; i <= maxW; i++) {
+        // check hit
+        var curB = w1[i];
+        if (curB.c == 0) {
+            ctx.beginPath();
+            ctx.rect(lx, ly, 6, 6);
+            ctx.fillStyle = '#FF0000';
+            ctx.fill();
+            ctx.closePath();
+        }
+        if (i % wpl == 0) {
+            ly = ly + wp;
+            lx = lx - (wpl * wp);
+        }
+        lx = lx + wp;
+    }
+
+    // right
+    lx = 236;
+    ly = 70;
+    for (var i = 1; i <= maxW; i++) {
+        // check hit
+        var curB = w2[i];
+        if (curB.c == 0) {
+            ctx.beginPath();
+            ctx.rect(lx, ly, 6, 6);
+            ctx.fillStyle = '#0000FF';
+            ctx.fill();
+            ctx.closePath();
+        }
+        if (i % wpl == 0) {
+            ly = ly + wp;
+            lx = lx - (wpl * wp);
+        }
+        lx = lx + wp;
     }
 }
 
